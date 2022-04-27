af8ca7f2a4bf108f22d40621c9a86cf96f55a5fb
ayush <jtplartisan@gmail.com>
2022-04-27 13:32:06 +0530

    Card

--- src/Component/geners.js
+++ src/Component/geners.js
@@ -1,17 +1,47 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-const Geners=()=>{
-    return(
-        <Navbar bg="light" variant="light">
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import CardP from "./card";
+import { useEffect, useState } from "react";
+import axios from "axios";
+const Geners = () => {
+
+
+  const [video, setVideo] = useState([])
+
+  useEffect(() => {
+    var url = "http://localhost:5000/video";
+    axios.get(url).then(res => {
+      const video = res.data;
+      setVideo(video);
+    })
+  }, []);
+  console.log(video)
+
+
+  return (
+    <>
+      <Navbar bg="light" variant="light">
         <Container>
 
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
-    )
+      <Container>
+        <Row>
+          <Col >
+            {video.map((item, index) => {
+              return (
+                <CardP img={item.imgurl} title={item.title} />
+              )
+            })}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
 }
 export default Geners;
